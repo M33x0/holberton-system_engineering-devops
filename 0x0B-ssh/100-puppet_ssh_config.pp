5a089738bd7da4e394013efcf725af0166796f38
M33x0 <michael.mbea@gmail.com>
2021-08-17 13:01:55 +0200

    client config file with puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+# configure ssh config file
+file_line{'Turn off password auth':,
+    path =>'/etc/ssh/ssh_config',
+    line =>'PasswordAuthentication no',
+}
+file_line{'Declare identity file':,
+    path =>'/etc/ssh/ssh_config',
+    line =>'IdentityFile ~/.ssh/holberton',
+}
